db052391610a98693c3df43b44e233ce694b3f21
Otis <otis@hdtech.com.tw>
2014-08-09 11:39:24 +0800

    add flash session

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
   def self.from_omniauth(auth)
-    binding.pry
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
