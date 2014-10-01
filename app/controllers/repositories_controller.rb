0a68b4803c4dab87c3106069aeea7fd9c9f87c62
Otis <otis@hdtech.com.tw>
2014-10-02 00:58:26 +0800

    Add css structure

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   before_action :set_repo, only: [ :show ]
   def index
-    Repository.refresh_by_user(current_user)
+    # Repository.refresh_by_user(current_user)
     @repos = current_user.repositories
   end
 
